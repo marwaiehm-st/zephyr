3b06de14d5bd685b2912584016f85eef743df3ff
Erwan Gouriou <erwan.gouriou@st.com>
2024-06-07 12:03:22 +0200

    boards: st: nucleo_l657x0_q: Sign and load on ext flash if FSBL
    In case of FSBL configuration, add a post build procedure to generate
    a signed bin file using STM32MP_SigningTool_CLI (expected to be in PATH
    along with stm32cubeprogrammer flashing CLI tool).
    Only working on Linux for now.
    
    Then, use stm32cubeprogrammer extload option to load the generated bin
    on board ext flash.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@st.com>

--- boards/st/nucleo_n657x0_q/board.cmake
+++ boards/st/nucleo_n657x0_q/board.cmake
@@ -1,4 +1,21 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (c) 2024 STMicroelectronics
 
-# To be added
+if(CONFIG_STM32N6_FSBL)
+set_property(GLOBAL APPEND PROPERTY extra_post_build_commands
+  COMMAND STM32MP_SigningTool_CLI
+  -bin ${PROJECT_BINARY_DIR}/${CONFIG_KERNEL_BIN_NAME}.bin
+  -nk -of 0x80000000 -t fsbl -hv 2.1
+  -o ${PROJECT_BINARY_DIR}/${CONFIG_KERNEL_BIN_NAME}_signed.bin
+  -dump ${PROJECT_BINARY_DIR}/${CONFIG_KERNEL_BIN_NAME}_signed.bin
+  WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+)
+
+set_property(TARGET runners_yaml_props_target PROPERTY bin_file zephyr_signed.bin)
+
+board_runner_args(stm32cubeprogrammer "--port=swd")
+board_runner_args(stm32cubeprogrammer "--tool-opt= mode=HOTPLUG ap=1")
+board_runner_args(stm32cubeprogrammer "--extload=MX25UM51245G_STM32N6570-NUCLEO.stldr")
+
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
+endif()
