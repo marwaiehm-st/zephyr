a958ccd553ed03614f0582cc0296d03f99c31c97
Erwan Gouriou <erwan.gouriou@st.com>
2024-06-12 16:35:54 +0200

    boards: st: stm32n6: Enable signing tool on Windows machines
    Be a little bit more smart and allow building and flashing on Windows.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@st.com>

--- boards/st/stm32n6570_dk/board.cmake
+++ boards/st/stm32n6570_dk/board.cmake
@@ -2,8 +2,15 @@
 # Copyright (c) 2024 STMicroelectronics
 
 if(CONFIG_STM32N6_FSBL)
+
+if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL Windows)
+    set(SIGNING_TOOL STM32MP_SigningTool_CLI.exe)
+else()
+    set(SIGNING_TOOL STM32MP_SigningTool_CLI)
+endif()
+
 set_property(GLOBAL APPEND PROPERTY extra_post_build_commands
-  COMMAND STM32MP_SigningTool_CLI
+  COMMAND ${SIGNING_TOOL}
   -bin ${PROJECT_BINARY_DIR}/${CONFIG_KERNEL_BIN_NAME}.bin
   -nk -of 0x80000000 -t fsbl -hv 2.1
   -o ${PROJECT_BINARY_DIR}/${CONFIG_KERNEL_BIN_NAME}_signed.bin
